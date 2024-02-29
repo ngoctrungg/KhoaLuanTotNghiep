a13f589475d8fd6f8782c3fc527b3ca84cf4d29f
NgocTrung <ngoctrungg02@gmail.com>
2024-02-29 14:38:24 +0700

    thêm layout và một vài chức năng

---
+++ ViewModels/HangHoaVM.cs
@@ -0,0 +1,23 @@
+﻿namespace KLTN_E.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string TenLoai { get; set; }
+    }
+    public class ChiTietHangHoaVM
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string TenLoai { get; set; }
+        public string MoTa { get; set; }
+        public int DiemDanhGia { get; set; }
+        public int SoLuongTon { get; set; }
+        public string ChiTiet { get; set; }
+    }
+}
