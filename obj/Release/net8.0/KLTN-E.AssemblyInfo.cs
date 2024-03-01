777d4efbccde94bd9f2ecfd526221c00178b5fe3
thag15 <2000003730@nttu.edu.vn>
2024-03-01 11:26:43 +0700

    update hoquocthang

--- obj/Release/net8.0/KLTN-E.AssemblyInfo.cs
+++ obj/Release/net8.0/KLTN-E.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("KLTN-E")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13f589475d8fd6f8782c3fc527b3ca84cf4d29f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KLTN-E")]
