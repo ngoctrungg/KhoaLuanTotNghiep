fc394c722c37ce86488e221504a13a599552b99e
NgocTrung <ngoctrungg02@gmail.com>
2024-04-18 10:57:25 +0700

    Thêm phân trang, sửa phần Category thành dạng tree

--- obj/Debug/net8.0/KLTN-E.AssemblyInfo.cs
+++ obj/Debug/net8.0/KLTN-E.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KLTN-E")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde678f645979a8f3d3910b42d6de96fe6a31b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KLTN-E")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KLTN-E")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
